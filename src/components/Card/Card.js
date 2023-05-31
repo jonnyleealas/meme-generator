ce84b07a988d1389578440389919a811000ebba3
jonathon lee <jonnyleealas@gmail.com>
2023-05-31 11:45:14 -0500

    play with onMouseOver hover

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,19 @@
 import './card.css'
 import Fry from '../Card/cardImage/memeimg.png'
 const Card = () => {
+
+    const clickMe = () => {
+        return alert('hello')
+    }
+
     return (
         <div className='card'>
             <div className='input-container'>
-
-            
-                <input className='input-1' type='text' />
-        
-            
-                <input className='input-2' type='text' />
-            
+                <input className='input-1' type='text' placeholder=' type some stuff' />
+                <input className='input-2' type='text' placeholder=" type some stuff" />
             </div>
-        
-                
-            <button className='button'>Get a new meme image</button>
-            
-        
-                <img className='image' src={Fry} alt='fry' />
-            
-            
+            <button className='button' onClick={clickMe} type='button'>Get a new meme image</button>
+            <img className='image' src={Fry} alt='fry' onMouseOver={clickMe} />
         </div>
     )
 }
