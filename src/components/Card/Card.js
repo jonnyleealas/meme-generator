482859cb02ff71a2afadbda3cba113a1072efade
jonathon lee <jonnyleealas@gmail.com>
2023-07-03 21:31:40 -0500

    added grey on hover to login button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,27 +4,33 @@ import Memes from '../../memeData'
 
 
 const Card = () => {
-  const randomFirstMeme = Memes[Math.floor(Math.random() * Memes.length)].image
+  const firstRandomImage = Memes[Math.floor(Math.random() * Memes.length)].image
   const [memeState, setMemeState] = useState({
-    topText: '',
-    bottomText: '',
-    memeImage: randomFirstMeme
+    topName: '',
+    bottomName: '',
+    memeImage: firstRandomImage
   })
-  
+
   const [allMemes, setAllMemes] = useState(Memes)
+  console.log(Memes)
 
   const handleClick = () => {
-    const randomImage = allMemes[Math.floor(Math.random() * allMemes.length)].image
-     setMemeState(randomImage)
-   }
+    const newMeme = allMemes[Math.floor(Math.random() * allMemes.length)].image
+
+    setMemeState(prevMeme => ({
+      ...prevMeme,
+      memeImage: newMeme
+    }))
+  }
+
     return (
         <div className='card'>
             <div className='input-container'>
                 <input className='input-1' type='text' placeholder=' type some stuff' />
                 <input className='input-2' type='text' placeholder=" type some stuff" />
             </div>
-            <button className='button' type='button' onClick={handleClick}>Get a new meme image</button>
-             <img className='image' src={memeState.memeImage ? memeState.memeImage : memeState} alt='fry' />
+            <button className='button' type='button' onClick={handleClick} >Get a new meme image</button>
+             <img className='image' src={memeState.memeImage} alt='fry' />
            </div> 
     )
 }
