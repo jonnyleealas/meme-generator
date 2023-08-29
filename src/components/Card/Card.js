060e8c673a6204f8eb1adc0e4c6760bddf5e0414
Jonathon Lee <jonnyleealas@gmail.com>
2023-08-29 15:34:35 -0500

    Configured api imgflip

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './card.css'
 import Memes from '../../memeData'
 
 
 const Card = () => {
-  const firstRandomImage = Memes[Math.floor(Math.random() * Memes.length)].image
   const [memeState, setMemeState] = useState({
     topText: '',
     bottomText: '',
-    memeImage: firstRandomImage,
+    memeImage: "https://i.imgflip.com/1ihzfe.jpg",
   })
-
-  const [allMemes, setAllMemes] = useState(Memes)
-
+  
+  const [allMemes, setAllMemes] = useState([])
+  
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+  },[])
+  
+  console.log(allMemes)
 
   const handleChange = (e) => {
     const { name, type, value } = e.target
@@ -24,16 +30,17 @@ const Card = () => {
     })
   }
 
-  console.log(memeState)
 
   const handleClick = () => {
-    const newMeme = allMemes[Math.floor(Math.random() * allMemes.length)].image
+    const newMeme = allMemes[Math.floor(Math.random() * allMemes.length)].url
 
     setMemeState(prevMeme => ({
       ...prevMeme,
       memeImage: newMeme
     }))
+    console.log(newMeme,"new Meme")
   }
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -63,7 +70,10 @@ const Card = () => {
       <button className='button' type='button' onClick={handleClick} >Get a new meme image</button>
       <button className='button' type='button' onClick={handleSubmit} >Submit</button>
       <div className="memeWrapper">
+   
+
       <img className='image' src={memeState.memeImage} alt='fry' />
+     
       <div className='topText'>{memeState.topText}</div>
       <div className='bottomText'>{memeState.bottomText}</div>
       </div>
